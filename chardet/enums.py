5c58bb1d89f4e6f29c37163dd450317851a7a6b6
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:14:10 -0800

    refactor: Improve readability and formatting of enums in chardet/enums.py

--- chardet/enums.py
+++ chardet/enums.py
@@ -1,22 +1,22 @@
-"""
-All of the Enums that are used throughout the chardet package.
+"""All of the Enums that are used throughout the chardet package.
 
 :author: Dan Blanchard (dan.blanchard@gmail.com)
 """
 
+
 class InputState:
-    """
-    This enum represents the different states a universal detector can be in.
-    """
+    """This enum represents the different states a universal detector can be in."""
+
     PURE_ASCII = 0
     ESC_ASCII = 1
     HIGH_BYTE = 2
 
+
 class LanguageFilter:
-    """
-    This enum represents the different language filters we can apply to a
+    """This enum represents the different language filters we can apply to a
     ``UniversalDetector``.
     """
+
     CHINESE_SIMPLIFIED = 1
     CHINESE_TRADITIONAL = 2
     JAPANESE = 4
@@ -26,26 +26,26 @@ class LanguageFilter:
     CHINESE = CHINESE_SIMPLIFIED | CHINESE_TRADITIONAL
     CJK = CHINESE | JAPANESE | KOREAN
 
+
 class ProbingState:
-    """
-    This enum represents the different states a prober can be in.
-    """
+    """This enum represents the different states a prober can be in."""
+
     DETECTING = 0
     FOUND_IT = 1
     NOT_ME = 2
 
+
 class MachineState:
-    """
-    This enum represents the different states a state machine can be in.
-    """
+    """This enum represents the different states a state machine can be in."""
+
     START = 0
     ERROR = 1
     ITS_ME = 2
 
+
 class SequenceLikelihood:
-    """
-    This enum represents the likelihood of a character following the previous one.
-    """
+    """This enum represents the likelihood of a character following the previous one."""
+
     NEGATIVE = 0
     UNLIKELY = 1
     LIKELY = 2
@@ -54,15 +54,22 @@ class SequenceLikelihood:
     @classmethod
     def get_num_categories(cls):
         """:returns: The number of likelihood categories in the enum."""
-        return len([attr for attr in dir(cls) if not attr.startswith("__") and isinstance(getattr(cls, attr), int)])
+        return len(
+            [
+                attr
+                for attr in dir(cls)
+                if not attr.startswith("__") and isinstance(getattr(cls, attr), int)
+            ]
+        )
+
 
 class CharacterCategory:
-    """
-    This enum represents the different categories language models for
+    """This enum represents the different categories language models for
     ``SingleByteCharsetProber`` put characters into.
 
     Anything less than CONTROL is considered a letter.
     """
+
     UNDEFINED = 255
     LINE_BREAK = 254
     SYMBOL = 253
